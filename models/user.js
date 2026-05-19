147f8bda82cd23791c2811f928ed4797eba50c0a
COSMICBUG <54317137+Cosm1cBug@users.noreply.github.com>
2026-05-19 17:11:00 +0530

    Schema typo fix

--- models/user.js
+++ models/user.js
@@ -1,23 +1,38 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    apiKey: String,
+    apiKey: {
+        type: String,
+        index: true
+    },
     keyId: {
         type: String,
         index: true
     },
-    keyHash: string,
-    name: String,
+    keyHash: {
+        type: String
+    },
+    name: {
+        type: String
+    },
     email: { 
         type: String, 
-        unique: true 
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
     },
-    password: String,
+    password: {
+        type: String,
+        required: true
+    },
     status: { 
         type: String, 
         default: "basic" 
     },
-    endDate: Date,
+    endDate: {
+        type: Date
+    },
     image: { 
         type: String, 
         default: "default.jpg" 
@@ -30,7 +45,7 @@ const UserSchema = new mongoose.Schema({
         type: Number, 
         default: 0 
     },
-    updateAt: Date
+    updatedAt: Date
 });
 
 UserSchema.index({ email: 1 })
